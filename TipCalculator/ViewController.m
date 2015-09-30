0c5cfb843859ab78e1e99069980f26e78af52a48
Michael Chang <michael@micbase.com>
2015-09-30 14:17:13 -0500

    Finish the calculation logic

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,13 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipRate;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmount;
+- (IBAction)onTap:(id)sender;
+- (IBAction)onAmountChange:(id)sender;
+- (void)updateValues;
 @end
 
 @implementation ViewController
@@ -22,6 +29,26 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onTap:(id)sender {
+    [self updateValues];
+}
+
+- (IBAction)onAmountChange:(id)sender {
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billAmount = [self.billAmount.text floatValue];
+    
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    
+    float tipAmount = billAmount * [tipValues[self.tipRate.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    
+    self.tipAmount.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalAmount.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
 }
 
 @end
