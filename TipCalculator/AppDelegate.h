07b98b4f295f65839d46d071764a75878b4ce884
Michael Chang <michael@micbase.com>
2015-09-30 13:44:55 -0500

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Michael Chang on 9/30/15.
+//  Copyright © 2015 Michael Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
