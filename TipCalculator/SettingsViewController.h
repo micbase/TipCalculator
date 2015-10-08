bdb5e7173c6efe32b7179f9f16fb760184109bcc
Michael Chang <michael@micbase.com>
2015-10-08 00:03:39 -0500

    Add a setting view

---
+++ TipCalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  TipCalculator
+//
+//  Created by Michael Chang on 10/7/15.
+//  Copyright © 2015 Michael Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
